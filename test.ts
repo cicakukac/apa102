32b3984c2903ad933e2e0195a53fb918de3c2c7e
cicakukac <wirth.ervin.daniel@gmail.com>
2025-03-02 00:00:55 +0000

    Update pxt.json, main.blocks, test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,6 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+let ctrl = new apa102.p()
+for (let i = 0; i < 25; ++i) {
+    ctrl.plotAt(3,3)
+    basic.pause(500)
+}
