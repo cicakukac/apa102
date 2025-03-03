39ed0c53801d6f10a4a8616a8dc08110ac96adc7
cicakukac <wirth.ervin.daniel@gmail.com>
2025-03-03 19:59:46 +0000

    Update main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,15 @@
 namespace apa102 {
+    //% blockId=apa102p
     export class p {
+        private static defaultInstance: p; // Static instance for automatic usage
+
         NUM_PIXELS_X: number
         NUM_PIXELS_Y: number
         DAT: number
         CLK: number
         NUM_PIXELS: number
         BRIGHTNESS: number
+        pixels: number[][]
 
         constructor() {
             this.NUM_PIXELS_X = 16;
@@ -16,10 +20,10 @@ namespace apa102 {
             this.BRIGHTNESS = 7;
 
             // Initialize pixel array
-            let pixels: number[][] = this.init_pixels();
+            this.pixels = this.init_pixels();
         }
 
-        init_pixels(): number[][] {
+        private init_pixels(): number[][] {
             let pixel_list: number[][] = [];
             for (let i = 0; i < this.NUM_PIXELS; i++) {
                 pixel_list.push([0, 0, 0, this.BRIGHTNESS]);
@@ -28,17 +32,26 @@ namespace apa102 {
         }
 
         /**
-         * Addddd
+         * Get the default instance (auto-created)
+         */
+        private static getInstance(): p {
+            if (!this.defaultInstance) {
+                this.defaultInstance = new p(); // Automatically create instance
+            }
+            return this.defaultInstance;
+        }
+
+        /**
+         * Plot a pixel at position X, Y
          */
         //% blockId=apa102plotat
         //% block="plot at $x $y"
-        //% x.min=0 x.max=4
-        //% y.min=0 y.max=4
-        plotAt(x: number, y: number): void {
-            console.log("NUM_PIXELS_X: " + this.NUM_PIXELS_X);
-            // this.NUM_PIXELS_X = (this.NUM_PIXELS_X + 1) % this.NUM_PIXELS_X;
+        //% x.min=0 x.max=15
+        //% y.min=0 y.max=15
+        static plotAt(x: number, y: number): void {
+            let instance = this.getInstance(); // Get or create instance
+            console.log("NUM_PIXELS_X: " + instance.NUM_PIXELS_X);
             led.plot(x, y)
         }
     }
 }
-
